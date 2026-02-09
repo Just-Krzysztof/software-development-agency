b2ecb91c6870affeccf63ffcfcf151f42406b245
Krzysztof <krzysztofjustgdynia@gmail.com>
2026-02-09 17:18:53 +0100

    navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,25 @@
+import Image from 'next/image'
+import Logo from '../../../public/Logo.png'
+
 export const Navbar = () => {
-  return <>Navbar</>;
-};
+  return (
+    <div className="border-b-2 border-sda-gray py-2 md:py-4">
+      <div className="max-w-[1400px] w-full mx-auto  flex justify-between">
+        <Image
+          src={Logo}
+          alt="Just_dev logo of agency"
+          className="w-25 md:w-35"
+        ></Image>
+        <ul className="flex items-center justify-between gap-4 text-md md:text-lg">
+          <li className="cursor-pointer">Welcome</li>
+          <li className="cursor-pointer">O mnie</li>
+          <li className="cursor-pointer">Usługi</li>
+          <li className="cursor-pointer">Projekty</li>
+        </ul>
+        <button className="py-2 px-4 bg-sda-blue rounded-xl shadow-card cursor-pointer">
+          Wycena
+        </button>
+      </div>
+    </div>
+  )
+}
